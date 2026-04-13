5b9dfb2607fdd0b5c9076072e7a90a0623cf2702
Zarz Official <zarzofficial@example.com>
2026-04-13 05:30:19 +0200

    Deploy: Update base path and add GitHub Actions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/',
+    base: '/zarzofficial-up/',
     build: {
       outDir: 'docs',
     },
